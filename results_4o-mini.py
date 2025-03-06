0508d87539480ce21a8434e978536f03b2fe88b1
pcbrom <pcbrom@gmail.com>
2025-03-06 10:40:45 -0300

    update

--- results_4o-mini.py
+++ results_4o-mini.py
@@ -13,30 +13,28 @@ load_dotenv(dotenv_path=dotenv_path)
 
 # Access and store the environment variable
 openai_api_key = os.getenv("OPENAI_API_KEY")
-model = 'gpt-4o-mini-2024-07-18'
+model_name = 'gpt-4o-mini-2024-07-18'
 
-# Import model
+# Load the CSV file
 csv_file = "augmented_prompt.csv"
 df = pd.read_csv(csv_file, decimal='.', sep=',', encoding='utf-8')
-df = df[df['model'] == model]
-cols_to_fill = ['results', 'score']
-df[cols_to_fill] = df[cols_to_fill].fillna('')
 print(df)
 
 # Iterate over each row and make API call
-output_filename = f"experimental_design_results_{model}.csv"
-for index, row in tqdm(df.iterrows(), total=len(df), desc=f"Processing {model}"):
+for index, row in tqdm(df.iterrows(), total=len(df)):
     if index % 100 == 0 and index != 0:
         print("min. pause...")
         time.sleep(60)
     try:
         augmented_prompt = row['augmented_prompt']
-
+        temperature = 0.3
+        top_p = 0.3
+        
         response = openai.chat.completions.create(
-            model=model,
+            model=model_name,
             messages=[{"role": "user", "content": augmented_prompt}],
-            temperature=float(row['temperature']),
-            top_p=float(row['top_p'])
+            temperature=temperature,
+            top_p=top_p
         )
 
         # Extract and store the generated text
@@ -44,11 +42,14 @@ for index, row in tqdm(df.iterrows(), total=len(df), desc=f"Processing {model}")
         df.loc[index, 'results'] = generated_text
 
     except openai.OpenAIError as e:
-        print(f"Error processing row {index} for model {model}: {e}")
+        print(f"Error processing row {index} for model {model_name}: {e}")
         df.loc[index, 'results'] = f"Error: {e}"  # Store the error message
     except Exception as e:
-        print(f"Unexpected error processing row {index} for model {model}: {e}")
+        print(f"Unexpected error processing row {index} for model {model_name}: {e}")
         df.loc[index, 'results'] = f"Error: {e}"
 
-# Save the updated DataFrame (optional)
+# Save the updated DataFrame
+output_filename = f"results/experimental_design_results_{model_name}.csv"
 df.to_csv(output_filename, index=False)
+
+print(f"Completed. Results saved in {output_filename}")
